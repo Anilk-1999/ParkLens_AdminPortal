58746fba25bbe6d50602d5f8070e9101e4fb98aa
Anilk-1999 <129037878+Anilk-1999@users.noreply.github.com>
2024-01-10 15:28:58 +0530

    Update AttributeSteps.java

--- src/test/java/stepDefinitions/AttributeSteps.java
+++ src/test/java/stepDefinitions/AttributeSteps.java
@@ -1,10 +1,7 @@
 package stepDefinitions;
 
 import java.io.IOException;
-
-
 import org.testng.Assert;
-
 import io.cucumber.java.en.*;
 import pageObjects.TestAttributes;
 import testBase.Baseclass;
@@ -15,22 +12,27 @@ public class AttributeSteps extends Baseclass
 	@Then("Make sure that the attribute information page should appears")
 	public void make_sure_that_the_attribute_information_page_should_appears() throws InterruptedException 
 	{   
-		//driver=Baseclass.getDriver();
+	    //driver=Baseclass.getDriver();
 	    attributes=new TestAttributes(driver);
-		Assert.assertEquals(attributes.getAttributeTabText(), attributes.getHomePageHeaderLable());
+	    Assert.assertEquals(attributes.getAttributeTabText(), attributes.getHomePageHeaderLable());
 	}
 
 	@Then("Verify i should navigate to the add attributes page")
 	public void verify_i_should_navigate_to_the_add_attributes_page() throws InterruptedException 
 	{
-		Assert.assertTrue(true, attributes.getAddPageHeaderLable());
+	     Assert.assertTrue(true, attributes.getAddPageHeaderLable());
 	}
 
+
+/*--------------------------------------------------Data driven testing Using excel data-----------------------------------------------------------------*/
+
+
+	
 //	@When("Fill all the required attribute information:")
 //	public void fill_all_the_required_attribute_information() throws IOException, InterruptedException 
 //	{
 //		
-//        String path="./excel-datas/AttributeInformation.xlsx";
+//              String path="./excel-datas/AttributeInformation.xlsx";
 //		
 //		Xlxutility utilitydata=new Xlxutility(path);
 //		int totalRow=utilitydata.getRowCount("Sheet1");
@@ -38,7 +40,7 @@ public class AttributeSteps extends Baseclass
 //		for(int r=1;r<=totalRow;r++)
 //		{
 //			
-//			 String exp="https://parklens-admin.tech-active.com/settings/attributes";
+//		     String exp="https://parklens-admin.tech-active.com/settings/attributes";
 //		     String act=driver.getCurrentUrl();
 //		    
 //		   
@@ -51,7 +53,7 @@ public class AttributeSteps extends Baseclass
 //			String attributeName=utilitydata.getCellData("Sheet1", r, 0);
 //			String attributeDesc=utilitydata.getCellData("Sheet1", r, 1);
 //			
-//		    attributes.setAttributeName(attributeName);
+//	        attributes.setAttributeName(attributeName);
 //	        attributes.setDescription(attributeDesc);
 //	        Thread.sleep(1000);
 //	        adminUser=new TestAddAdminUsers(driver);
@@ -65,7 +67,7 @@ public class AttributeSteps extends Baseclass
 	@When("Fill all the required attribute information:")
 	public void fill_all_the_required_attribute_information() throws IOException, InterruptedException 
 	{
-		    attributes.setAttributeName("Zone");
+		attributes.setAttributeName("Zone");
 	        attributes.setDescription("zone title");
 	        Thread.sleep(1000);
 //	        adminUser=new TestAddAdminUsers(driver);
@@ -83,7 +85,6 @@ public class AttributeSteps extends Baseclass
 	@Then("The admin should be redirected to attribute details page")
 	public void the_admin_should_be_redirected_to_attribute_details_page() throws InterruptedException 
 	{
-		System.out.println("anil kumar");
 		Assert.assertTrue(true, attributes.getHomePageHeaderLable());
 	}
 	
@@ -93,7 +94,7 @@ public class AttributeSteps extends Baseclass
 	@Then("Verify i should navigate to the edit attribute page")
 	public void verify_i_should_navigate_to_the_edit_attribute_page() throws InterruptedException
 	{
-		attributes=new TestAttributes(driver);
+	    attributes=new TestAttributes(driver);
 	    Assert.assertEquals(attributes.getEditPageHeaderLable(), "Edit Attributes");
 	}
 
@@ -106,11 +107,11 @@ public class AttributeSteps extends Baseclass
 	@Then("The attribute should successfully updated with confirmation message")
 	public void the_attribute_should_successfully_updated_with_confirmation_message() throws InterruptedException 
 	{
-		Assert.assertTrue(attributes.ConfirmationMassageDisplayed());
+	    Assert.assertTrue(attributes.ConfirmationMassageDisplayed());
 	    System.out.println("anil : "+attributes.ConfirmationMassageDisplayed());
 	}
 	
-/*-----------------------------Attribute update----------------------------------*/
+/*-----------------------------Attribute Deletion----------------------------------*/
 	
 	@Then("The attribute should successfully deleted with confirmation message")
 	public void The_attribute_should_successfully_deleted_with_confirmation_message() throws InterruptedException
